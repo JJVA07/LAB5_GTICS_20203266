3966be7617fac76eed845ab3e276648778af9502
JJVA07 <a20203266@pucp.edu.pe>
2025-05-15 20:43:05 -0500

    Primer commit

---
+++ LAB5_GTICS_20203266/src/test/java/com/example/lab5_gtics_20203266/Lab5Gtics20203266ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_gtics_20203266;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5Gtics20203266ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
